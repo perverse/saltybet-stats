a7a06891c418231a430e3e4c7366a047bac208b8
Ronnie Pyne <ronnie.pyne@creditorwatch.com.au>
2020-01-06 17:09:49 +1100

    Match resource

--- app/Resources/Match/Full.php
+++ app/Resources/Match/Full.php
@@ -3,6 +3,7 @@
 namespace App\Resources\Character;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Resoures\Character\Full as CharacterResource;
 
 class Full extends JsonResource
 {
@@ -10,15 +11,42 @@ class Full extends JsonResource
     {
         $return = [
             'id' => $this->resource->id,
-            'name' => $this->resource->name,
-            'wins' => $this->resource->wins,
-            'losses' => $this->resource->losses,
-            'total' => $this->resource->wins + $this->resource->losses
+            'strategy' => $this->resource->strategy,
+            'prediction' => $this->resource->prediction,
+            'tier' => $this->resource->tier,
+            'mode' => $this->resource->mode,
+            'odds' => $this->resource->odds,
+            'time' => $this->resource->time,
+            'date' => $this->resource->date->format('Y-m-d H:i:s'),
         ];
 
-        if ($this->resource->relationLoaded('matches')) {
+        if ($this->resource->relationLoaded('character_a')) {
             $return = array_merge([
-                'matches' => $this->collection($this->resource->matches)
+                'character_a' => CharacterResource::make($this->resource->character_a)
+            ], $return);
+        }
+
+        if ($this->resource->relationLoaded('character_b')) {
+            $return = array_merge([
+                'character_b' => CharacterResource::make($this->resource->character_b)
+            ], $return);
+        }
+
+        if ($this->resource->relationLoaded('winner')) {
+            $return = array_merge([
+                'winner' => CharacterResource::make($this->resource->winner)
+            ], $return);
+        }
+
+        if ($this->resource->relationLoaded('crowd_favourite')) {
+            $return = array_merge([
+                'crowd_favourite' => CharacterResource::make($this->resource->crowd_favourite)
+            ], $return);
+        }
+
+        if ($this->resource->relationLoaded('illum_favourite')) {
+            $return = array_merge([
+                'illum_favourite' => CharacterResource::make($this->resource->illum_favourite)
             ], $return);
         }
 
